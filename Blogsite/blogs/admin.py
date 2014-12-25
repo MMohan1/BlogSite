8b180ed9fdbd39cf300ea0998c14e1db6b3a892f
Manmohan Sharma <manmohansharma987@gmail.com>
2014-12-25 22:41:36 +0530

    Adding Admin Details in Blog Site Project

---
+++ Blogsite/blogs/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from blogs.models import Blog, Action
+
+
+class ActionInline(admin.StackedInline):  # StackedInline for line by line
+    model = Action
+    extra = 1
+
+
+class BlogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Blog Details', {'fields': [('Blog_Title', 'Blog_Field')]}),
+        ('Blog Description', {'fields': ['URL', 'Blog_Description']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ActionInline]
+    list_display = ('Blog_Title', 'Blog_Field', 'URL')
+    list_filter = ['Blog_Field']
+    search_fields = ['Blog_Title']
+admin.site.register(Blog, BlogAdmin)
